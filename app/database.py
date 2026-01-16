155d4577dc0b4f4a5cd0cdff6333258988720fb1
ljspinelli <ljspinelli.pa@gmail.com>
2026-01-16 20:21:48 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./notas.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////opt/render/project/src/notas.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -10,3 +10,4 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
